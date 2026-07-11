b4ea7e2c4e7222ff8c12612b2288db876bf36da9
triyog-shrestha <triyogshrestha86@gmail.com>
2026-07-11 17:13:07 +0545

    a simple cpp calculator

---
+++ calculator.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+
+int add(int a, int b);
+int diff(int a, int b);
+int mul(int a, int b);
+float quo(float a, float b);
+void intro();
+void options();
+
+int main(){
+    intro();
+    while (true){
+        int x,y,op;
+        char ch;
+
+        cout<<"Enter a number : ";
+        cin >> x;
+        cout<<"Enter another number : ";
+        cin>>y;
+        options();
+        cin >> op;
+
+    
+        if (op==1){
+            cout<<"Sum of " << x <<" and " << y << " = " << add(x,y)<<endl;
+        }
+        else if (op==2){
+            cout<<"Difference of " << x <<" and " << y << " = " << diff(x,y)<<endl;
+        }
+        else if (op==3){
+            cout<<"Product of " << x <<" and " << y << " = " << mul(x,y)<<endl;
+        }
+        else if (op==4){
+            cout<<"Quotient of " << x <<" and " << y << " = " << quo(x,y)<<endl;
+        }
+        else{
+            cout << "An error occurred" <<endl;
+        }
+        cout<<"Do you want to continue?(y/n) : ";
+        cin>>ch;
+
+        if (ch=='n' || ch=='N'){
+            break;
+        }
+    }
+}
+
+
+
+
+int add(int a, int b){
+    return a+b;
+}
+
+int diff(int a, int b){
+    return a-b;
+}
+
+int mul(int a, int b){
+    return a*b;
+}
+
+float quo(float a, float b){
+    return a/b;
+}
+
+
+void intro(){
+    cout << "This is a simple calculator" << endl ;
+}
+
+void options(){
+    cout << "Press 1 for addition " << endl;
+    cout << "Press 2 for subtraction " << endl;
+    cout << "Press 3 for multiplication " << endl;
+    cout << "Press 4 for division " << endl;
+}
